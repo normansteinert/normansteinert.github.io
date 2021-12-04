073146550fc173d89b5e073f990eeef61fe9112f
normansteinert <87495753+normansteinert@users.noreply.github.com>
2021-12-05 00:52:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function citeNJS2021b() {
+  alert("@Article{e2021GL094273,\n AUTHOR = {Steinert, N. J., J. F. Gonz\'alez-Rouco, C. A. Melo Aguilar, F. Garc\'{\i}a-Pereira, E. Garc\'{\i}a-Bustamante, P. de Vrese, V. Alexeev, J. H. Jungclaus, S. J. Lorenz, S. Hagemann,},\n TITLE = {{Agreement of Analytical and Simulation-Based Estimates of the Required Land Depth in Climate Models}},\n JOURNAL = {Geophysical Research Letters},\n VOLUME = {48},\n YEAR = {2021},\n NUMBER = {20},\n NOTE = {e2021GL094273},\n URL = {https://agupubs.onlinelibrary.wiley.com/doi/10.1029/2021GL094273},\n DOI = {10.1029/2021GL094273}\n}");
+}
+
+function citeNJS2021a() {
+  alert("@Article{JHM-D-21-0023.1,\n AUTHOR = {Steinert, N. J., J. F. Gonz\'alez-Rouco, P. de Vrese, E. Garc\'{\i}a-Bustamante, S. Hagemann, C. Melo-Aguilar, J. H. Jungclaus, and S. J. Lorenz},\n TITLE = {{Increasing the Depth of a Land Surface Model. Part II: Temperature Sensitivity to Improved Subsurface Thermodynamics and Associated Permafrost Response}},\n JOURNAL = {Journal of Hydrometeorology},\n VOLUME = {22},\n YEAR = {2021},\n NUMBER = {12},\n PAGES = {3231--3254},\n URL = {https://journals.ametsoc.org/view/journals/hydr/aop/JHM-D-21-0023.1/JHM-D-21-0023.1.xml},\n DOI = {10.1175/JHM-D-21-0023.1}\n}");
+}
+
+function citeJFGR2021() {
+  alert("@Article{JHM-D-21-0024.1,\n AUTHOR = {Gonz\'alez-Rouco, J. F., N. J. Steinert, E. Garc\'{\i}a-Bustamante, S. Hagemann, P. de Vrese, J. H. Jungclaus, S. J. Lorenz, C. Melo-Aguilar, F. Garc\'{\i}a-Pereira, and J. Navarro},\n TITLE = {{Increasing the Depth of a Land Surface Model. Part I: Impacts on the Subsurface Thermal Regime and Energy Storage}},\n JOURNAL = {Journal of Hydrometeorology},\n VOLUME = {22},\n YEAR = {2021},\n NUMBER = {12},\n PAGES = {3211--3230},\n URL = {https://journals.ametsoc.org/view/journals/hydr/aop/JHM-D-21-0024.1/JHM-D-21-0024.1.xml},\n DOI = {10.1175/JHM-D-21-0024.1}\n}");
+}
+
+function citeCMA2020() {
+  alert("@Article{cp-16-453-2020,\n AUTHOR = {Melo-Aguilar, C. and Gonz\'alez-Rouco, J. F. and Garc\'{\i}a-Bustamante, E. and Steinert, N. and Jungclaus, J. H. and Navarro, J. and Rold\'an-G\'omez, P. J.},\n TITLE = {Methodological and physical biases in global to subcontinental borehole temperature reconstructions: an assessment from a pseudo-proxy perspective},\n JOURNAL = {Climate of the Past},\n VOLUME = {16},\n YEAR = {2020},\n NUMBER = {2},\n PAGES = {453--474},\n URL = {https://cp.copernicus.org/articles/16/453/2020/},\n DOI = {10.5194/cp-16-453-2020}\n}");
+}
+
+function citeCMA2018() {
+  alert("@Article{cp-14-1583-2018,\n AUTHOR = {Melo-Aguilar, C. and Gonz\'alez-Rouco, J. F. and Garc\'{\i}a-Bustamante, E. and Navarro-Montesinos, J. and Steinert, N.},\n TITLE = {Influence of radiative forcing factors on ground--air temperature coupling during the last millennium: implications for borehole climatology},\n JOURNAL = {Climate of the Past},\n VOLUME = {14},\n YEAR = {2018},\n NUMBER = {11},\n PAGES = {1583--1606},\n URL = {https://cp.copernicus.org/articles/14/1583/2018/},\n DOI = {10.5194/cp-14-1583-2018}\n}");
+}
